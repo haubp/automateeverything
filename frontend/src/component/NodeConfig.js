c853f4202cde35841339bb83ca2db89e8ff6dc9f
haubui <haubui@HAU-BUI-LT.local>
2022-10-05 01:00:35 +0700

    design node

--- frontend/src/component/NodeConfig.js
+++ frontend/src/component/NodeConfig.js
@@ -1,31 +1,66 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import './NodeConfig.css';
+import Card from "react-bootstrap/Card";
+import Dropdown from "react-bootstrap/Dropdown";
+
+import "./NodeConfig.css";
 
 export default function () {
   return (
-    <div className="nodeBoard">
-        <Card style={{ width: '18rem' }}>
+    <>
+      <div className="nodeBoard">
+        <Card style={{ width: "18rem" }}>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>Node ID</Card.Title>
+            <div className="form-group mt-3">
+              <label>Category</label>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Dropdown Button
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Something else
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+            <div className="form-group mt-3">
+              <label>Group</label>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Dropdown Button
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Something else
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+            <div className="form-group mt-3">
+              <label>Test Cases</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter test cases"
+              />
+            </div>
           </Card.Body>
         </Card>
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-    </div>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Run
+      </button>
+    </>
   );
 }
